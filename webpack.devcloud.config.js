c8fbc953c25256f93600d83b3fb059c6e49de61f
kefimov <ya.kefimov@yandex.ru>
2016-01-10 17:42:09 +0000

    Added stage-0 to support ES7

--- webpack.devcloud.config.js
+++ webpack.devcloud.config.js
@@ -12,7 +12,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react', "stage-0"]
+          presets: ['es2015', 'react', "stage-0"] 
         }
       },
     ]
